ef4a3c3c41f2b6ca0c0bcead5ae1db5296f78433
Karabarsik <kamchatka.09@bk.ru>
2023-04-16 19:10:34 +0300

    project

---
+++ script.js
@@ -0,0 +1,67 @@
+const boxes = document.querySelectorAll(".box");
+const mole = document.querySelector(".mole");
+const restart = document.getElementById("restart");
+const timeCounter = document.getElementById("time");
+const scoreCounter = document.getElementById("score");
+
+let score = 0;
+let currentBox = 0;
+let islocked = false;
+let currentTime = 10;
+
+function showMole(){
+    boxes.forEach((box) =>{
+        box.classList.remove("mole");
+    });
+    islocked = false;
+    let randomBox = boxes[Math.floor(Math.random() * 9)];
+
+    randomBox.classList.add("mole");
+
+    currentBox = randomBox.id
+}
+
+function start(){
+    score = 0;
+    currentTime = 10;
+    moleTimer = setInterval(showMole, 300);
+    timeTimer = setInterval(countTime, 1000);
+
+}
+
+start();
+
+boxes.forEach((box) =>{
+    box.addEventListener("click", () =>{
+        if(box.id == currentBox){
+            if(islocked) return;
+
+            score++;
+            scoreCounter.innerHTML = score;
+            box.classList.remove("mole");
+            islocked = true;
+        }
+    });
+});
+
+function countTime(){
+    currentTime--;
+    timeCounter.innerHTML = currentTime;
+
+    if(currentTime == 0){
+        clearInterval(timeTimer);
+        clearInterval(moleTimer);
+        alert('GAME OVER! \n YOUR SCORE:' + score);
+    }
+}
+
+function restartGame() {
+    clearInterval(timeTimer);
+    clearInterval(moleTimer);
+    start();
+}
+
+restart.addEventListener("click", restartGame);
+
+
+
